c430f4f24d685402457bc4383dd012ebb08ae311
Sem <semmer99@gmail.com>
2018-12-06 16:10:14 +0100

    Finished splash screen, added how to play button, finished design game screen

---
+++ SplashJS.js
@@ -0,0 +1,8 @@
+function instructionsDropDwn() {
+    var x = document.getElementById("playinstructions");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
