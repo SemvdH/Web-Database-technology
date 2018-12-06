d559e15f8a7f2bb2f616e1408f026fb3d4ead1db
DanDanBro <45453605+DanDanBro@users.noreply.github.com>
2018-12-06 19:18:41 +0100

    tried out something didn't work

--- battleship/public/javascripts/GameJS.js
+++ battleship/public/javascripts/GameJS.js
@@ -15,7 +15,8 @@ var space = 1;
 for (var r=0; r<10; r++) {
   var col = "";
   for (var c=0; c<10; c++) { 
-    col += "<td data-pos='"+space+"'></td>"; space++; 
+    col += "<td data-pos='"+space+"'></td>"; 
+    space++; 
   }
   $("#player").append("<tr>"+col+"</tr>");
   $("#opponent").append("<tr>"+col+"</tr>");
