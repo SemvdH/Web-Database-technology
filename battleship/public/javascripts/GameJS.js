a2a8946176f08ea28b227c2e6347c2800f9ee91b
Sem <semmer99@gmail.com>
2018-12-06 21:13:08 +0100

    added timer, probeerde kleur veranderen door klikken maar werkt alleen met css, begonnen met ships indicator

--- battleship/public/javascripts/GameJS.js
+++ battleship/public/javascripts/GameJS.js
@@ -1,22 +1,41 @@
-// var space = 1;
-// for (var r = 0; r < 10; r++) {
-//     var col = "";
-//     for (var c = 0; c < 10; c++) {
-//         col += "<td data-pos='" + space + "'></td>"; space++;
-//     }
-//     $("#player").append("<tr>" + col + "</tr>");
-// }
-
-// $('document').ready(function() {
-
-// });
 
 var space = 1;
 for (var r=0; r<10; r++) {
   var col = "";
   for (var c=0; c<10; c++) { 
-    col += "<td data-pos='"+space+"'></td>"; space++; 
+    col += "<td></td>"; space++; 
+  } 
+  // data-pos='"+space+"'
+  $(".player").append("<tr>"+col+"</tr>");
+  $(".opponent").append("<tr>"+col+"</tr>");
+}
+
+$( function changeColor() {
+  var cell = ".player tr td";
+  $('.player tr td').click(function () {
+    //$(this).css('background-color', '#aaa');
+    // $(this).css('border', '2px solid black');
+    $(this).toggleClass('clicked');   //doesn't want to work?
+  });
+});
+
+// timer
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
   }
-  $("#player").append("<tr>"+col+"</tr>");
-  $("#opponent").append("<tr>"+col+"</tr>");
 }
