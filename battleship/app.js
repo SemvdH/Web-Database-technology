8a1e65b3e0a1c96d4f9a1ff1768fdb2b6d065057
Sem <semmer99@gmail.com>
2018-12-08 14:44:41 +0100

    set up server stuff, server now able to run and jumping from splash to game (GET methods) workd

--- battleship/app.js
+++ battleship/app.js
@@ -1,41 +1,78 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//------------code from assignment-------------
+var express = require("express");
+var http = require("http");
 
+
+
+var port = process.argv[2];
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+// app.use(express.static('public'));
+http.createServer(app).listen(port);
+//-----------------------------------------------
+const bodyparser = require("body-parser");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.sendFile("splash.html", {root: "./public"});
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// pressing play button returns game page
+app.get("/play", function(req, res, next) {
+  res.sendFile("game.html", {root: "./public"});
+});
 
-// catch 404 and forward to error handler
+
+
+//log each request made to server
 app.use(function(req, res, next) {
-  next(createError(404));
+  console.log('[LOG] %s\t%s\t%s\t%s', new Date().toISOString(), req.connection.remoteAddress,
+      req.method, req.url);
+  next(); // call on to next component
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//---------- auto generated code----------//
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
 
-module.exports = app;
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+ module.exports = app;
--- battleship/routes/index.js
+++ battleship/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile("splash.html", {root: "./public"});
+});
+
+// pressing play button returns game page
+router.get("/play", function(req, res, next) {
+  res.sendFile("game.html", {root: "./public"});
 });
 
 module.exports = router;
